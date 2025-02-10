7e9d7b218a9081be41977dfe5bf3f32817fde9a8
unknown <hieptram40@gmail.com>
2025-02-10 11:47:20 +0700

    updated auth service

--- src/main/java/com/hieptran/smarthome_server/Service/EmailService.java
+++ src/main/java/com/hieptran/smarthome_server/Service/EmailService.java
@@ -7,31 +7,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
+//import org.thymeleaf.TemplateEngine;
+//import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
 
-    private final TemplateEngine templateEngine;
+//    private final TemplateEngine templateEngine;
 
     public void sendVerificationEmail(String recipientEmail, 
                                       String recipientName, 
                                       String verificationCode, 
                                       int expirationMinutes) {
 
-        Context context = getContext(recipientName, verificationCode, expirationMinutes);
+//        Context context = getContext(recipientName, verificationCode, expirationMinutes);
 
-        String htmlContent = templateEngine.process("email.html", context);
+//        String htmlContent = templateEngine.process("email.html", context);
 
         MimeMessage message = mailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setTo(recipientEmail);
             helper.setSubject("Verify Your Email");
-            helper.setText(htmlContent, true); // true để gửi dưới dạng HTML
+            helper.setText("htmlContent", true); // true để gửi dưới dạng HTML
             mailSender.send(message);
         } catch (MessagingException e) {
             throw new RuntimeException("Failed to send verification email", e);
@@ -48,18 +48,18 @@ public class EmailService {
 
         mailSender.send(message);
     }
-
-    private Context getContext(String recipientName, String verificationCode, int expirationMinutes) {
-        Context context = new Context();
-        context.setVariable("logoUrl", "https://example.com/logo.png");
-        context.setVariable("recipientName", recipientName);
-        context.setVariable("verificationCode", verificationCode);
-        context.setVariable("expirationMinutes", expirationMinutes);
-        context.setVariable("verificationUrl", "https://example.com/verify?code=" + verificationCode);
-        context.setVariable("supportUrl", "https://home-pod.vercel.app/how-it-works");
-        context.setVariable("", "Thanh Xuan, Ha Noi, Viet Nam");
-        context.setVariable("settingsUrl", "https://example.com/settings");
-        return context;
-    }
+//
+//    private Context getContext(String recipientName, String verificationCode, int expirationMinutes) {
+//        Context context = new Context();
+//        context.setVariable("logoUrl", "https://example.com/logo.png");
+//        context.setVariable("recipientName", recipientName);
+//        context.setVariable("verificationCode", verificationCode);
+//        context.setVariable("expirationMinutes", expirationMinutes);
+//        context.setVariable("verificationUrl", "https://example.com/verify?code=" + verificationCode);
+//        context.setVariable("supportUrl", "https://home-pod.vercel.app/how-it-works");
+//        context.setVariable("", "Thanh Xuan, Ha Noi, Viet Nam");
+//        context.setVariable("settingsUrl", "https://example.com/settings");
+//        return context;
+//    }
 
 }
